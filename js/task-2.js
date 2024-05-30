ea88201d6827e4f63767bdd98c8ad8e30b205d82
Alexandr Solovey <158730378+Solovey260885@users.noreply.github.com>
2024-05-30 21:08:47 +0300

    start-finish HW6

---
+++ js/task-2.js
@@ -0,0 +1,31 @@
+class Storage {
+  #items;
+  constructor(items) {
+    this.#items = items;
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    const idxitemToRemove = this.#items.indexOf(itemToRemove);
+    if (idxitemToRemove !== -1) {
+      this.#items.splice(idxitemToRemove, 1);
+    }
+    return this.#items;
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
